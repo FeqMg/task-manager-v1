590c263ec8819887ecbe6f21ce26772e9506e836
Alan <alanslaeman@gmail.com>
2026-09-15 02:19:51 +0200

    new-user route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,29 @@
-import express from 'express'
+import express, { type Request, type Response } from 'express'
 
 const server = express();
 const port = 3000;
+
+server.use(express.json());
+
+server.post('/new-user', (req: Request, res: Response) => {
+    type Data = {
+        username: string,
+        email: string,
+        password: string,
+        confirmPassword: string
+    }
+
+    const data: Data = req.body;
+    try {
+        if (data.password != data.confirmPassword) {
+            return res.status(400).json({ msg: "Password do not match!" });
+        }
+        return res.status(200).json({ msg: "User created!" });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ msg: "Something went wrong.." });
+    }
+});
 
 server.listen(port, () => {
     console.log(`Server running on port: ${port}`);
